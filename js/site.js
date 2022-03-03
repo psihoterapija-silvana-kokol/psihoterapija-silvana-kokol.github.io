0ecb3c094e065463ae6df372f4988f3c4fc8f436
vodnibivol <vodnibivol@gmail.com>
2022-03-04 00:00:40 +0100

    removed jquery, added own animations

--- js/site.js
+++ js/site.js
@@ -1,34 +1,36 @@
 const Main = (function () {
-  /* --- rellax --- */
+  // cache DOM
+  const yearSpan = document.querySelector('#year');
+  const arrowDown = document.querySelector('.arrow-down');
+  let oMeniDiv = document.querySelector('.o-meni');
 
+  // connect listeners
+  arrowDown.addEventListener('click', scrollDown);
+  window.addEventListener('scroll', toggleArrow);
+
+  // init
   new Rellax('.rellax', { center: true });
+  yearSpan.innerText = new Date().getFullYear();
 
-  $('#year').text(new Date().getFullYear());
-
-  /* --- scrolling arrow --- */
-
-  $('.arrow-down').click(function () {
+  // f(x)
+  function scrollDown() {
     let scrollOffset;
 
-    if ($('.o-meni').height() < $(window).height()) {
-      // scroll to middle
-      scrollOffset = $('.o-meni').offset().top - ($(window).height() - $('.o-meni').height()) / 2;
+    if (oMeniDiv.offsetHeight < window.innerHeight) {
+      // scroll to middle of section
+      scrollOffset = Math.floor(oMeniDiv.offsetTop - (window.innerHeight - oMeniDiv.offsetHeight) / 2);
     } else {
-      scrollOffset = $('.o-meni').offset().top;
+      // scroll to top of section
+      scrollOffset = oMeniDiv.offsetTop;
     }
 
-    $('html, body').animate({ scrollTop: scrollOffset }, 1000);
+    scroll2(scrollOffset, 1600, 'easeInOutCubic');
+  }
 
-    $('.arrow-down').fadeOut();
-  });
-
-  $(window).scroll(function () {
-    if (window.scrollY === 0) {
-      $('.arrow-down').fadeIn();
-    } else {
-      $('.arrow-down').fadeOut();
-    }
-  });
+  function toggleArrow() {
+    if (window.scrollY > 0) arrowDown.classList.add('transparent');
+    else arrowDown.classList.remove('transparent');
+  }
 })();
 
 // TODO: fonti so lahko skriti, ko se nalozijo, se pocasi pojavijo (animacija)
